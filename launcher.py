8a3af3be5158a0697f1a742d4689075e46afe682
mafagafogigante <mafagafogigante@gmail.com>
2014-08-07 17:05:05 -0300

    tests and shit

--- launcher.py
+++ launcher.py
@@ -1,6 +1,8 @@
 from engine import *
 from player import Player
 from nature import Bat
+from nature import Wolf
+from nature import Bear
 
 
 def main():
@@ -8,7 +10,11 @@ def main():
     player.build()
     # please, write a plot for this
     bat = Bat(2)
+    wolf = Wolf(1)
+    bear = Bear(1)
     fight(player, bat)
+    fight(player, wolf)
+    fight(player, bear)
 
 if __name__ == '__main__':
     main()
--- nature.py
+++ nature.py
@@ -18,3 +18,39 @@ class Bat(creature.Creature):
         # 0 is attack
         # [0][1] gets the attack_function
         self.skills[0][1](self, target)
+
+
+class Wolf(creature.Creature):
+    def __init__(self, level):
+        super(Wolf, self).__init__("Wolf")
+        self.life = 15 + 10 * level
+
+        self.gold = level * 5
+
+        self.strength = 6 + 3 * level
+
+        self.exp = level ** 2 * 4
+        self.lvl = level
+
+    def play(self, target):
+        # 0 is attack
+        # [0][1] gets the attack_function
+        self.skills[0][1](self, target)
+
+
+class Bear(creature.Creature):
+    def __init__(self, level):
+        super(Bear, self).__init__("Bear")
+        self.life = 15 + 15 * level
+
+        self.gold = level + 5 * 3
+
+        self.strength = 8 + 3 * level
+
+        self.exp = level ** 2 * 4
+        self.lvl = level
+
+    def play(self, target):
+        # 0 is attack
+        # [0][1] gets the attack_function
+        self.skills[0][1](self, target)
